474b23e2617a817df26f6f0ce75f0e86d620482d
david <david20250515@gmail.com>
2025-06-19 12:03:10 -0400

    Create api.del.sh

---
+++ api.del.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /opt/dep/api
+
+sudo rm -rf /opt/www/api/admin-api
+sudo mv /opt/dow/api/admin-api /opt/www/api
+
+sudo chown -R xxx:www-data /opt/www
+sudo chmod -R 755 /opt/www
+sudo chmod +x /opt/www/api/admin-api
+
+sudo systemctl restart admin-api.service
+
+rm -rf /opt/dow/api/admin-api
